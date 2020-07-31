8329f386b7d3bc3ba1712150761dde4a7244b01e
JulyEstrada <juli.03_@hotmail.com>
2020-07-31 15:51:23 -0500

    Update platzi-mongo

---
+++ Learn/FundamentosC#-NetCore/CoreEscuela/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CoreEscuela
+{
+    class Escuela // Plantilla
+    {
+        //Atributos
+        private string nombre;
+        private string direccion;
+        private int añoFundacion; // Pulicultural 
+
+        //Encapsulamiento
+        public string Nombre
+        { 
+            get{ return nombre;}  
+            set{ nombre = value;}
+        }
+        public string Direccion{ get; set;}
+        public int AñoFundacion{ get; set;}
+
+        //Metodos o Funciones
+        public void Timbrar(){ Console.Beep(2000,3000); }
+
+    }
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var miEscuela = new Escuela();
+            miEscuela.Nombre = "Platzi";
+            miEscuela.Direccion = "Calle 128 sur";
+            miEscuela.AñoFundacion = 1993;
+            Console.WriteLine("Timbrandooo");
+            miEscuela.Timbrar();
+            
+        }
+    }
+}
---
+++ Learn/FundamentosC#-NetCore/CoreEscuela2/Entidades/TiposEscuela.cs
@@ -0,0 +1,9 @@
+namespace  CoreEscuela2.Entidades
+{
+    public enum TiposEscuela
+    {
+        Primaria,
+        Secundaria,
+        PreEscolar
+    }
+}
