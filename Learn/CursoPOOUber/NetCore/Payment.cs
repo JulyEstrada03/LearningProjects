4696bd6a0d638b6f871a0464ba696650edae5d55
JulyEstrada <juli.03_@hotmail.com>
2021-08-02 14:34:57 -0500

    Net core changes and platzi mongo modified

--- Learn/CursoPOOUber/NetCore/Payment.cs
+++ Learn/CursoPOOUber/NetCore/Payment.cs
@@ -3,5 +3,15 @@ namespace NetCore
     public class Payment
     {
         int id;
+        private int myVar;
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+         public Payment(Parameters)
+         {
+             
+         }  
     }
 }
